4eda1d7151619abee819f5048d7d21c713a8109f
Tharusha Induwara <tharusha.induwar@gmail.com>
2021-08-27 13:12:17 +0530

    Begin development of JavaScript part

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Script Loaded!")
+
+var blackBtn = document.getElementById("black-btn");
+var blueBtn = document.getElementById("blue-btn");
+var redBtn = document.getElementById("red-btn");
+var purpleBtn = document.getElementById("purple-btn");
+var roseBtn = document.getElementById("rose=btn");
+
+var timeBtn = document.getElementById("time-btn");
+var heartBtn = document.getElementById("heart-btn");
+var butBtn = document.getElementById("buy-btn");
